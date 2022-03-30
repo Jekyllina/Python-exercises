a0a028f5727782e7e6f1a43254e4957e29ef63f4
Jekyllina <dianasternini@hotmail.it>
2022-03-31 01:47:52 +0200

    first commit

---
+++ Circle_main.py
@@ -0,0 +1,30 @@
+from circle import Circle
+
+if __name__ == "__main__":
+    circles = []
+    circle = Circle(radius = -4)    
+    print(str(circle))
+    circles.append(circle)
+
+    circle2 = Circle(radius = 2.5)
+    print(str(circle2))
+    circles.append(circle2)
+
+    print(f"The Circles are equals? {circle == circle2}")
+    
+    print(f"circle with area {circle.area} is bigger than circle2 with area {circle2.area}? {circle > circle2}")
+
+    circle3 = Circle(diameter = 2)
+    circles.append(circle3)
+
+    circle4 = Circle(radius = 7)
+    circles.append(circle4)
+
+    circle5 = Circle(diameter = 6)
+    circles.append(circle5)
+
+    circles.sort(key=lambda x : x.area)
+
+    print("\nCircles after sorting:")
+    for circle in circles:
+        print(str(circle)) 
---
+++ circle.py
@@ -0,0 +1,31 @@
+import math
+
+class Circle:
+    def __init__(self, radius = None, diameter = None):
+        if radius is not None:
+            self.radius = abs(radius)
+            self.diameter = self.radius * 2
+        elif diameter is not None:
+            self.diameter = abs(diameter) 
+            self.radius = self.diameter * 0.5
+        elif radius is None and diameter is None:
+            raise TypeError("Define a radius or a diameter for the circle")           
+
+        self.area = math.pi * math.pow(self.radius, 2)
+
+    def __str__(self):
+        return f"Circle radius: {self.radius}, Circle diameter: {self.diameter}, Circle area: {self.area}"
+
+    def __add__(self, other):
+        newCircle = Circle(self.radius + other.radius, self.diameter + other.diameter)
+        newCircle.area = self.area + other.area
+        return newCircle
+
+    def __eq__(self, other):
+        return (self.area == other.area)
+
+    def __lt__(self, other):
+        return (self.area < other.area)
+
+    def __gt__(self, other):
+        return (self.area > other.area)  
